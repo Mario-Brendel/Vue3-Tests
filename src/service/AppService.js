c7f5a4bed69a7040bb48f1801e196deeb66d1643
mario <mario.brendel1990@gmail.com>
2020-01-04 00:34:36 +0100

    removed unnecessary service

--- src/service/AppService.js
+++
@@ -1,7 +0,0 @@
-class AppService {
-    render() {
-        console.log("render");
-    }
-}
-export const appService = new AppService();
-//# sourceMappingURL=AppService.js.map
--- src/service/AppService.ts
+++
@@ -1,7 +0,0 @@
-class AppService {
-    render() {
-        console.log("render");
-    }
-}
-
-export const appService: AppService = new AppService()
